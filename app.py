2371874f871f1b456f34a322affd2f29c397de45
Carlos Tovar <c.tovarb@uniandes.edu.co>
2023-02-23 22:07:55 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import urllib.request, json
+
+import os
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def get_movies():
+    url = "http://localhost:3000/estado"
+
+    response = urllib.request.urlopen(url)
+    status = response.status
+
+    return '', status
+
+
+if __name__ == '__main__':
+    app.run()
